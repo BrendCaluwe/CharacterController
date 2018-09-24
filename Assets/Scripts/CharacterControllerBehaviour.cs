3c6a27d015cce8d6719dcfc4ef7946ac0b9b035a
BrendCaluwe <43540787+BrendCaluwe@users.noreply.github.com>
2018-09-24 16:36:11 +0200

    CharacterController Debug
    lijntje geschreven voor debug

--- Assets/Scripts/CharacterControllerBehaviour.cs
+++ Assets/Scripts/CharacterControllerBehaviour.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Assertions;
 using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
 
 public class CharacterControllerBehaviour : MonoBehaviour {
 
+    private CharacterController _characterController;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        _characterController = GetComponent<CharacterController>();
+#if DEBUG
+        Assert.IsNotNull(_characterController, "amai goe kees,geen characterController");
+#endif
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+
 }
